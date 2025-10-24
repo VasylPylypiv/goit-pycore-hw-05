c849055d4bbfa5c7e51ca47f5452ba9e3df779b6
Vasyl Pylypiv <vasyl.pylypiv@ringteam.com>
2025-10-24 14:50:07 +0300

    Task 2 ready

---
+++ task2.py
@@ -0,0 +1,19 @@
+import re
+
+
+def generator_numbers(text: str):
+    pattern = r'\d+(?:\.\d+)?'
+    numbers = re.findall(pattern, text)
+    for number in numbers:
+        yield float(number)
+
+
+def sum_profit(text: str, func: callable):
+    total = 0
+    for number in func(text):
+        total += number
+    return total
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
