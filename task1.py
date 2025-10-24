d9fa0ac81dcb1c481bbacb8de02ef85a1bf69d94
Vasyl Pylypiv <vasyl.pylypiv@ringteam.com>
2025-10-24 14:12:06 +0300

    Task1 ready

---
+++ task1.py
@@ -0,0 +1,20 @@
+def caching_fibonacci():
+    cache = {}
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
+        if n in cache:
+            return cache[n]
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci
+  
+# Отримуємо функцію fibonacci
+fib = caching_fibonacci()
+
+# Використовуємо функцію fibonacci для обчислення 10-го числа Фібоначчі
+print(fib(10))  # Виведе 55
